a94c72aca5d74a16bb5bf54a328a9558ea9548f3
Maksim <ingenmech@gmail.com>
2020-10-17 16:11:37 +0300

    Delete exception

--- src/main/java/by/epam/evm/book/data/dao/specification/SpecificationFactory.java
+++ src/main/java/by/epam/evm/book/data/dao/specification/SpecificationFactory.java
@@ -1,7 +1,6 @@
 package by.epam.evm.book.data.dao.specification;
 
 import by.epam.evm.book.data.dao.Field;
-import by.epam.evm.book.data.dao.InputException;
 
 public class SpecificationFactory {
 
@@ -22,7 +21,7 @@ public class SpecificationFactory {
                 specification = new IdSpecification();
                 break;
             default:
-                throw new InputException("Field not exist in the listing" + " = " + field);
+                throw new IllegalArgumentException("Field not exist in the listing" + " = " + field);
         }
         return specification;
     }
